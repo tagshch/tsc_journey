f1bc00f886c522f2d41265fdee58a697e367a1e6
tagshch <t_shch@list.ru>
2017-01-18 22:30:11 +0300

    [handbook] function in-process

--- src/5_functions.ts
+++ src/5_functions.ts
@@ -53,8 +53,8 @@ the type if you have types on one side of the equation but not the other:
 
 let myPlus = function(x:number, y:number): number { return x + y; };
 
-let myPlus: (baseVal: number, plusVal: number) => number =
-	function(x, y) { return x + y; }
+// let myPlus: (baseVal: number, plusVal: number) => number =
+//	function(x, y) { return x + y; }
 
 /* ---
 This is called “contextual typing”, a form of type inference. 
@@ -165,6 +165,78 @@ console.log(
 console.log('\n==================\n');
 
 /* ======== THIS ======== */
+
+// --- this and arrow functions ---
+
+/* --- 
+Arrow functions capture the this where the function is 
+created rather than where it is invoked:
+--- */
+
+let simple_deck = {
+	suits: ['hearts', 'spades', 'clubs', 'diamonds'],
+	cards: Array(52),
+	createCardPicker: function(){
+		return () => {
+			let pickedCard = Math.floor(Math.random() * 52);
+			let pickedSuit = Math.floor(pickedCard / 13);
+
+			return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
+		}
+	}
+}
+
+let simpleCardPicker = simple_deck.createCardPicker();
+let simplePickedCard = simpleCardPicker();
+
+console.log("card:", simplePickedCard.card, "of", simplePickedCard.suit);
+
+// --- this parameters ---
+
+interface Card {
+	suit: string;
+	card: number;
+}
+interface Deck {
+	suits: string[];
+	cards: number[];
+	createCardPicker(this: Deck): () => Card;
+}
+
+let deck:Deck = {
+	suits: ['hearts', 'spades', 'clubs', 'diamonds'],
+	cards: Array(52),
+	createCardPicker: function(this:Deck){
+		return ()=>{
+			let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+		}
+	}
+}
+
+let cardPicker = deck.createCardPicker();
+let pickedCard = cardPicker();
+
+console.log("card: " + pickedCard.card + " of " + pickedCard.suit + '\n');
+
+// --- simple of above ---
+
+let bingo = {
+	getBingo:function(this:void){
+		return () =>{
+			console.log('BINGO!', this);
+		}
+	}	
+};
+
+let bongo = bingo.getBingo();
+bongo();
+
+
+// --- this parameters in callbacks ---
+
 
 
 console.log('\n==================\n');
