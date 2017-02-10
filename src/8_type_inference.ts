9a4cc6a328bc2705794f2ebf285ba84b6549c430
Tagir Schedrolyubov <t_shch@list.ru>
2017-02-10 10:29:36 +0300

    [handbook] type_inference in-process

--- src/8_type_inference.ts
+++ src/8_type_inference.ts
@@ -5,8 +5,14 @@ let _x = 3;
 
 /* ===== BEST COMMON TYPE ===== */
 
-/* ===== CONTEXTUAL TYPE ===== */
+/*
+When a type inference is made from several expressions, 
+the types of those expressions are used to calculate a “best common type”. 
+*/
 
+let _y = [0, 1, null];
+
+/* ===== CONTEXTUAL TYPE ===== */
 
 
 
